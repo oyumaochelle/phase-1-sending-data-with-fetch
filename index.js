024f3027bf5e97932846cc8e726eb262dbb4b590
ochelle <alexizochelle@gmail.com>
2024-01-09 21:37:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email
+    };
+    const config = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+    };
+
+    return fetch("http://localhost:3000/users", config)
+        .then(response => response.json())
+        .then(data => {
+        const newId = data.id;
+        document.body.innerHTML += newId;
+    })
+        .catch(error => {
+        document.body.innerHTML += error.message;
+        throw error;
+    });
+}
